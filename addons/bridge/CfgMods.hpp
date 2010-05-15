9442154d38abe4a52a4ba860d8298cffe805d931
Sickboy <sb@dev-heaven.net>
2010-05-15 16:02:30 +0200

    Mods

---
+++ addons/bridge/CfgMods.hpp
@@ -0,0 +1,33 @@
+	class CfgMods
+	{
+		class CBA
+		{
+			dir = "@CBA";
+			name = "Community Base Addons";
+			picture = "ca\ui\data\logo_arma2ep1_ca.paa";
+			hidePicture = "true";
+			hideName = "true";
+			actionName = "Website";
+			action = "http://dev-heaven.net";
+		};
+		class ACE
+		{
+			dir = "@ACE";
+			name = "Core - Advanced Combat Environment";
+			picture = "ca\ui\data\logo_arma2ep1_ca.paa";
+			hidePicture = "true";
+			hideName = "true";
+			actionName = "Website";
+			action = "http://ace.dev-heaven.net";
+		};
+		class ACEX
+		{
+			dir = "@ACEX";
+			name = "Xtras - Advanced Combat Environment";
+			picture = "ca\ui\data\logo_arma2ep1_ca.paa";
+			hidePicture = "true";
+			hideName = "true";
+			actionName = "Website";
+			action = "http://ace.dev-heaven.net";
+		};
+	};
