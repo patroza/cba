86ef88041febcdde212caed279ccbeab8769fb0f
Patrick Roza <roza@sixnetworks.net>
2012-12-18 11:46:36 +0100

    mod.cpp then

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -27,17 +27,6 @@ class CfgPatches {
 	};
 };
 
-class CfgMods {
-	class PREFIX {
-		dir = "@CBA_CO";
-		name = "Community Base Addons: CO (A2/OA)";
-		picture = "x\cba\addons\main\logo_cba_ca.paa";
-		actionName = "Website";
-		action = "http://dev-heaven.net/projects/cca";
-		description = "Bugtracker: http://dev-heaven.net/projects/cca<br />Documentation: http://dev-heaven.net/projects/cca";
-	};
-};
-
 class CfgSettings {
 	class CBA {
 		class Caching {
---
+++ mod.cpp
@@ -0,0 +1,5 @@
+name = "Community Base Addons: CO (A2/OA)";
+picture = "x\cba\addons\main\logo_cba_ca.paa";
+actionName = "Website";
+action = "http://dev-heaven.net/projects/cca";
+description = "Bugtracker: http://dev-heaven.net/projects/cca<br />Documentation: http://dev-heaven.net/projects/cca";
