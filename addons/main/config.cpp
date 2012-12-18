8046b305de21f647e0aeb653a380c6338a65baca
Patrick Roza <roza@sixnetworks.net>
2012-12-18 11:36:57 +0100

    CBA_CO

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -29,8 +29,8 @@ class CfgPatches {
 
 class CfgMods {
 	class PREFIX {
-		dir = "@CBA";
-		name = "Community Base Addons";
+		dir = "@CBA_CO";
+		name = "Community Base Addons: CO (A2/OA)";
 		picture = "x\cba\addons\main\logo_cba_ca.paa";
 		actionName = "Website";
 		action = "http://dev-heaven.net/projects/cca";
